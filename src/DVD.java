4f924d039f8820cb42aa38aaab81baf50fc8a639
Sebenzile <sebenzilegabone@gmail.com>
2022-06-24 12:09:31 +0200

    learning arrays

---
+++ src/DVD.java
@@ -0,0 +1,32 @@
+/* public class Java
+//  {
+//     public static int plusMethod(int x, int y);
+//     return x+y;
+//     public static void main(String[] args) {
+//         int x = 4;
+//         int y =5;
+//         System.out.println(plusMethod(x, y));
+// }
+}*/ 
+
+DVD[] dvdCollection = new DVD[15];
+
+public class DVD{
+    public String name;
+    public int releaseYear;
+    public String director;
+
+    public DVD(String name,int releaseYear,String director)
+    {
+        this.name= name;
+        this.releaseYear= releaseYear;
+        this.director=director;
+    }
+public String toString(){
+    return this.name + ", directed by" + this.director + ", released in" + this.releaseYear;
+}
+
+public static void main(String[] args) {
+    System.out.println(dvdCollection.DVD[]);
+}
+    }
