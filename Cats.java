d96b90b3a32dcab4192e40da9d2618399061bae5
Sebenzile <sebenzilegabone@gmail.com>
2022-07-21 22:47:56 +0200

    Create Cats.java

---
+++ Cats.java
@@ -0,0 +1,14 @@
+
+public class Cats 
+{
+    String pattern = "Striped"; String colour = " is orange";
+    public static void main(String[] args)
+    {
+        Cats kitten = new Cats();
+        Cats tiger= new Cats();
+        System.out.println(kitten.pattern);
+        System.out.println( tiger.colour);
+    }
+}
+
+// add another class with tiger maybe and code that and from there also see what public static method I will put, also my main could maybe go to my cats then I have a differnt method for kitten so that I don't have to worry even if I comment it out.
